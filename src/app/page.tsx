4e39ea5d97c98ccfde5090ce2bbd5ffadc084560
Andra Athasyah <andraathasyah14@gmail.com>
2025-09-16 12:01:23 +0000

    untuk warning webstie ini masih tahap pengembangan itu permanen selalu m

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -157,7 +157,7 @@ export default function Home() {
       </header>
        
       <div className="flex flex-1 flex-col gap-4 p-4 md:grid md:grid-cols-12 md:gap-8 md:p-10">
-        <main className="md:col-span-9">
+        <main className="md:col-span-8 lg:col-span-9">
           {!userCountry && (
             <Alert variant="destructive" className="mb-6">
               <AlertTriangle className="h-4 w-4" />
@@ -211,7 +211,7 @@ export default function Home() {
           </div>
         </main>
 
-        <aside className="md:col-span-3">
+        <aside className="md:col-span-4 lg:col-span-3">
           <Card className="bg-card/80">
             <CardHeader>
               <CardTitle className="flex items-center gap-2">
